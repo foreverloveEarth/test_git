29030f7ad65292f87915765ad7c61fe8963c6a56
wlb_global <wlb_global@qq.com>
2018-11-14 17:04:10 +0800

    wlb改了注释

--- src/main/java/test1.java
+++ src/main/java/test1.java
@@ -1,12 +1,10 @@
-import sun.applet.Main;
-
 /**
  * @Author earth
  * @Time 2018/11/14 16:47
  * @Version 1.0
  */
 public class test1 {
-    //qweqweqeq
+    //wlb改了还看了
     public static void main(String[] args) {
         System.out.println("hello git!");
     }
