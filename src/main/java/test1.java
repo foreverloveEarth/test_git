1181b2a1da74cb18f1b21c98f6dc2a96cc1b27fd
wlb_global <wlb_global@qq.com>
2018-11-14 17:25:37 +0800

    欧哈哈

--- src/main/java/test1.java
+++ src/main/java/test1.java
@@ -4,16 +4,16 @@
  * @Version 1.0
  */
 public class test1 {
-    //wlb改了还看了666
+    //wlb改了还看了666哈
     public static void main(String[] args) {
-        System.out.println("hello git!");
-        sum(1,2);
+        System.out.println("hello git！!");
+        sum(1,22);
     }
     public static int sum(int a,int b){
-        System.out.println(a+b);
+        System.out.println("a+b="+(a+b));
         //哈哈哈哈哈哈哈
-        //哈你个鸡儿
-        return a+b;
+        //哈你个鸡儿哈哈欧
+        return a+b+1;
 
     }
 }
