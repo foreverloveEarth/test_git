ac5ed7cfcac7e3fc32e6a44c3169889a161dedac
wlb_global <wlb_global@qq.com>
2018-11-14 17:07:27 +0800

    增加方法

--- src/main/java/test1.java
+++ src/main/java/test1.java
@@ -7,5 +7,10 @@ public class test1 {
     //wlb改了还看了
     public static void main(String[] args) {
         System.out.println("hello git!");
+        add(5,2);
+    }
+
+    public static int add(int x,int y){
+        System.out.println("x+y="+x+y);
     }
 }
