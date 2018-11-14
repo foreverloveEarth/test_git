a7b51f782e2de04fdfebac14df26cf3c2cfa9da4
earth <earth@qq.com>
2018-11-14 16:48:33 +0800

    Initial commit

---
+++ src/main/java/test1.java
@@ -0,0 +1,13 @@
+import sun.applet.Main;
+
+/**
+ * @Author earth
+ * @Time 2018/11/14 16:47
+ * @Version 1.0
+ */
+public class test1 {
+    //qweqweqeq
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
